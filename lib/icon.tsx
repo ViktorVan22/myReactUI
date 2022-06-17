bf300e56c4e7122c63697a9cf570fa8c456a06e1
ViktorVan22 <zcg5368@163.com>
2022-06-17 18:57:50 +0800

    创建Icon组件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import './importIcons'
 
+// interface定义属性类型
 interface IconProps {
   name: string;
 }
 
-const Icon: React.FunctionComponent<IconProps> = (props) => {
+const Icon: React.FunctionComponent<IconProps> =(props)=>{
+
   return (
-    <span>
-      <svg>
-        <use xlinkHref={`#${props.name}`}/>
-      </svg>
-    </span>
-  );
-};
+    <span>{props.name}</span>
+  )
+}
 
 export default Icon;
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,9 +1,12 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import Icon from './icon';
+import ReactDOM from "react-dom";
+import React from "react";
+import Icon from "./icon";
 
-ReactDOM.render(<div>
-  <Icon name="wechat"/>
-  <Icon name="alipay"/>
-  <Icon name="qq"/>
-</div>, document.querySelector('#root'));
+ReactDOM.render(
+  <div>
+    <Icon name="wechat" />
+    <Icon name="alipay" />
+    <Icon name="qq" />
+  </div>,
+  document.querySelector("#root")
+);
